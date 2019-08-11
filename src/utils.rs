4ad827cd9cc9a9bfb521b4267affdf187a5d23fe
Kazuaki Ishiguro <kazuaki@couger.co.jp>
2019-08-11 14:40:16 +0900

    added multi thread

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,7 @@ use rand::distributions::Standard;
 use rand_pcg::Pcg64Mcg;
 
 pub fn new_u32_vec(n: usize) -> Vec<u32> {
-    let mut rng = Pcg64Mcg::from_seed([0; 16]);
+    let rng = Pcg64Mcg::from_seed([0; 16]);
     rng.sample_iter(&Standard).take(n).collect()
 }
 
